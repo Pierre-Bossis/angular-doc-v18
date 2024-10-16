768ed1ec202f80cd91fdeb39bc6f5ef6939ff318
Pierre Bossis <pbossis@hotmail.com>
2024-10-16 11:51:54 +0200

    premier commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Demo01Component } from './components/demos/demo01/demo01.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},
+    {path:'demo01', component: Demo01Component}
+];
